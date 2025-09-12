60b0b3be99ff27051bb2a1e80d3f579bb4eb7454
sukchul <tjrcjf410@snu.ac.kr>
2025-09-12 15:46:46 +0900

    asd

--- learning/experiments_dr_go1.sh
+++ learning/experiments_dr_go1.sh
@@ -3,14 +3,14 @@ wandb_project="Go1Handstand"
 task="Go1Handstand"
 use_wandb=false
 seed=$2 
-# CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=false use_wandb=$use_wandb
-# CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project custom_wrapper=false task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
-# CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="td3" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=false use_wandb=$use_wandb
+CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=false use_wandb=$use_wandb
+CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project custom_wrapper=false task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
+CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="td3" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=false use_wandb=$use_wandb
 CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="td3" seed=$seed wandb_project=$wandb_project custom_wrapper=false task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
 
-# # CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project randomizatoin=false task=$task eval_randomization=true use_wandb=$use_wandb
+# CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project randomizatoin=false task=$task eval_randomization=true use_wandb=$use_wandb
 # # CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="ppo" seed=$seed wandb_project=$wandb_project custom_wrapper=true task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
-# CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="sac" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=false use_wandb=$use_wandb
+CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="sac" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=false use_wandb=$use_wandb
 # # CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="sac" seed=$seed wandb_project=$wandb_project randomization=false task=$task eval_randomization=true use_wandb=$use_wandb
 # # CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="sac" seed=$seed wandb_project=$wandb_project custom_wrapper=true task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
-# CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="sac" seed=$seed wandb_project=$wandb_project custom_wrapper=false task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
+CUDA_VISIBLE_DEVICES=$gpu_id python train.py policy="sac" seed=$seed wandb_project=$wandb_project custom_wrapper=false task=$task eval_randomization=true use_wandb=$use_wandb asymmetric_critic=true
